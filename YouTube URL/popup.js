01330d422f762226cd0ce3e16d998b530409f4f0
Younes <younesetud@Gmail.Com>
2022-12-10 19:50:23 +0100

    YouTube URL

---
+++ YouTube URL/popup.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var checkButton = document.getElementById('geturlbtn');
+
+  checkButton.addEventListener('click', function() {
+  gettin_tab();
+
+  }, false);
+
+}, false);
+
+function gettin_tab() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var tab = tabs[0];
+    alert(tab.url);
+  });
+
+}
